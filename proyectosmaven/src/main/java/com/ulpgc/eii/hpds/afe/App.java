7f2e0b841554050ef8c6a186e27cc471f9eea49b
Angeles Fuentes Exposito <angeles.fuent.exp@gmail.com>
2017-11-07 20:10:31 +0000

    Primercommit

---
+++ proyectosmaven/src/main/java/com/ulpgc/eii/hpds/afe/App.java
@@ -0,0 +1,13 @@
+package com.ulpgc.eii.hpds.afe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
